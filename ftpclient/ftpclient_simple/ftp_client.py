8d999c4fb6ccd3314b410e83973681525a548b12
malimba <malimbageorgeyandanbon@gmail.com>
2020-12-07 16:34:26 -0500

    new

--- ftpclient/ftpclient_simple/ftp_client.py
+++ ftpclient/ftpclient_simple/ftp_client.py
@@ -4,8 +4,18 @@ import socket
 s = socket.socket()
 #assinging host and port
 host = input(str('what is the hostname of the device you will like to connect to?\n=> '))
-port = 8082
+port = 9558
 #connecting the hostname and port
 s.connect((host, port))
-
 print (f'Connected to {host}')
+data = s.recv(1024)
+print(data.decode())
+running = True
+while running:
+    #receive command list
+    command_list = s.recv(1024)
+    command_list = command_list.decode()
+    print('Commands list: ', command_list)
+    command = input(str('Enter command: '))
+    print('Processing')
+    s.send(command.encode())
--- ftpclient/ftpclient_simple/ftp_server.py
+++ ftpclient/ftpclient_simple/ftp_server.py
@@ -1,14 +1,36 @@
 import socket
-
+import random
+from pathlib import Path
 #initialising socket
 s = socket.socket()
 #assinging host and port
 host = socket.gethostname()
-port = 8082
+port = 9558
 print (f"FTP active\nHostname:{host}\nPort:{port}")
-#binding the hostname and port
-s.bind((host, port))
-#listen to just one connection at a time
-s.listen(1)
-conn, addr = s.accept()
-print (f'{addr} has just connected')
+try:
+    #binding the hostname and port
+    s.bind((host, port))
+    #listen to just one connection at a time
+    s.listen(1)
+    conn, addr = s.accept()
+    print (f'{addr} has just connected')
+    #send the user the base path and instructions
+    base_path = str(Path.home())
+    base_path_data_to_send = f'Base path of Host:{host}  is {base_path}\nYou may use simple linux commands such as "cd" to change directory'
+    conn.sendto(base_path_data_to_send.encode(), addr)
+    running = True
+    while running:
+        #send command list to user
+        commands = {'cd':'change directory'}
+        commands_as_str = ''
+        for command in commands.keys():
+            commands_as_str += f'{command} => {commands[command]} \n'
+        # print(commands_as_str)
+        #send commands
+        conn.sendto(commands_as_str.encode(), addr)
+        # receive commands
+        command = (conn.recv(1024)).decode()
+        print(command)
+except Exception as e:
+    print (e)
+    print('Try changing the port')
