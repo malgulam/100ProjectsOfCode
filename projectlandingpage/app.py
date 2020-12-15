6652d8d9f3b7300aaaaada24e7edf988a9b037e2
malimba <malimbageorgeyandanbon@gmail.com>
2020-12-15 18:01:34 -0500

    added projectlandingpage

---
+++ projectlandingpage/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, url_for
+
+#configure app
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
